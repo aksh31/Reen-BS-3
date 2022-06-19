295bb63fd81785c542f69fee0853c3b391bc8cbb
aksh31 <98390411+aksh31@users.noreply.github.com>
2022-06-19 16:36:35 +0530

    beautiful section complte

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,24 @@
 let cl = console.log;
 
-$(document).ready(function(){
-    $('#searchIcon').on('click', function(){
+$(document).ready(function () {
+    $('#searchIcon').on('click', function () {
         $('.mainHeader .search input').toggleClass('show');
+    })
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
     })
 })
